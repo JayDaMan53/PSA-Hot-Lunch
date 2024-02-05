66f082548ece0ef6a909da336dd8e708cf780437
JayDaMan53 <118361901+JayDaMan53@users.noreply.github.com>
2024-02-05 12:38:54 -0500

    Add files via upload

---
+++ LunchList.js
@@ -0,0 +1,111 @@
+if (!(lunchList && AB_Schedule)) {
+	var lunchList = {
+		7: {
+			23: "Grilled Cheese Sandwich",
+			24: "Buffalo Bone-in Wings / Teriyaki Bone-in Wings",
+			25: "Salisbury Steak",
+			26: "N/A",
+			27: "N/A",
+			28: "Orange Chicken / Teriyaki Chicken",
+			29: "Cheeseburger",
+			30: "Italian Meat Sauce",
+			31: "Buffalo Bone-in Wings / Teriyaki Bone-in Wings",
+			32: "Nacho Meat / Nacho Queso"
+		},
+		8: {
+			1: "Nacho Meat / Nacho Queso",
+			2: "N/A",
+			3: "N/A",
+			4: "N/A",
+			5: "Cheeseburger",
+			6: "Grilled Cheese Sandwich",
+			7: "Buffalo Bone-in Wings / Teriyaki Bone-in Wings",
+			8: "Salisbury Steak",
+			9: "N/A",
+			10: "N/A",
+			11: "Orange Chicken / Teriyaki Chicken",
+			12: "Cheeseburger",
+			13: "Italian Meat Sauce",
+			14: "Buffalo Bone-in Wings / Teriyaki Bone-in Wings",
+			15: "Nacho Meat / Nacho Queso",
+			16: "N/A",
+			17: "N/A",
+			18: "Cheeseburger",
+			19: "Cheeseburger",
+			20: "Smackers Chicken Bites",
+			21: "Buffalo Bone-in Wings / Teriyaki Bone-in Wings",
+			22: "Salisbury Steak",
+			23: "N/A",
+			24: "N/A",
+			25: "Orange Chicken / Teriyaki Chicken",
+			26: "Cheeseburger",
+			27: "Italian Meat Sauce",
+			28: "Buffalo Bone-in Wings / Teriyaki Bone-in Wings",
+			29: "Nacho Meat / Nacho Queso",
+			30: "N/A",
+		}
+	}
+	
+	var AB_Schedule = {
+		7: {
+			23: "A",
+			24: "B",
+			25: "C",
+			26: "A Weekend",
+			27: "A Weekend",
+			28: "A",
+			29: "B",
+			30: "A",
+			31: "B"
+		},
+		8: {
+			1: "C",
+			2: "A Weekend",
+			3: "A Weekend",
+			4: "Labor Day",
+			5: "A",
+			6: "B",
+			7: "A",
+			8: "B",
+			9: "A Weekend",
+			10: "A Weekend",
+			11: "A",
+			12: "B",
+			13: "A",
+			14: "B",
+			15: "C",
+			16: "A Weekend",
+			17: "A Weekend",
+			18: "A",
+			19: "B",
+			20: "A",
+			21: "B",
+			22: "C",
+			23: "A Weekend",
+			24: "A Weekend",
+			25: "A",
+			26: "B",
+			27: "A",
+			28: "B",
+			29: "C",
+			30: "A Weekend",
+		}
+	}
+
+	var ChickenSandwich = {
+		7: {
+			23: "",
+			24: "",
+			25: "",
+			26: "",
+			27: "",
+			28: "",
+			29: "",
+			30: "",
+			31: "",
+			32: ""
+		}
+	}
+	
+	var Version = "1.5.2"
+}
