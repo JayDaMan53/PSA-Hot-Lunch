49b1f8f24e815dcca415df79cf9552f3374df61f
JayDaMan53 <118361901+JayDaMan53@users.noreply.github.com>
2024-11-08 13:15:05 -0500

    Added teacher reminder system for attendance

--- optionsJS.js
+++ optionsJS.js
@@ -6,6 +6,8 @@ var Breakfast = document.querySelector("#Breakfast");
 var Mode = document.getElementById("Mode")
 var NotificationReminder = document.getElementById("Notification Reminder")
 var SaveButton = document.getElementById("Save")
+var Attendance = document.getElementById("Attendance Reminders")
+var Classes = document.getElementById("Classes")
 
 chrome.storage.local.get(["SettingSave"], function(SaveData) {
     if (SaveData.SettingSave) {
@@ -14,15 +16,30 @@ chrome.storage.local.get(["SettingSave"], function(SaveData) {
         Email.value = SaveData.SettingSave[2];
         Lunch1.selectedIndex = SaveData.SettingSave[4];
         Mode.selectedIndex = SaveData.SettingSave[5];
-        if (SaveData.SettingSave[6] == undefined) {
-            NotificationReminder.checked = false
-        } else {
-            NotificationReminder.checked = SaveData.SettingSave[6];
+        // notifs
+        NotificationReminder.checked = SaveData.SettingSave[6];
+        // teachers
+        Attendance.checked = SaveData.SettingSave[7];
+        Classes.hidden = !SaveData.SettingSave[7];
+
+        for (let i = 0; i <= 10; i++) {
+            const checkbox = document.getElementById("Classes").querySelectorAll('input[type="checkbox"]')[i];
+            if (checkbox) {
+                checkbox.checked = SaveData.SettingSave[8][i]
+            }
         }
     }
 });
 
 SaveButton.addEventListener('click', function() {
+    const checkedValues = [];
+    for (let i = 0; i <= 10; i++) {
+        const checkbox = document.getElementById("Classes").querySelectorAll('input[type="checkbox"]')[i];
+        if (checkbox) {
+            checkedValues[i] = checkbox.checked
+        }
+    }
+
     var SaveDataList = [
         FirstName.value,
         LastName.value,
@@ -30,9 +47,15 @@ SaveButton.addEventListener('click', function() {
         "null",
         Lunch1.selectedIndex,
         Mode.selectedIndex,
-        NotificationReminder.checked
+        NotificationReminder.checked,
+        Attendance.checked,
+        checkedValues
     ];
     chrome.storage.local.set({ SettingSave: SaveDataList }, function() {
         console.log("Done!");
     });
+});
+
+Attendance.addEventListener("change", function () {
+    Classes.hidden = !this.checked
 });
